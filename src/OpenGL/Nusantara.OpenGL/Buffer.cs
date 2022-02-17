e934b2d92498c83921ccb32eac351de1f8b72e7e
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-02-17 19:45:10 +0700

    Add Buffer and VertexArray class.

---
+++ src/OpenGL/Nusantara.OpenGL/Buffer.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+using Silk.NET.OpenGL;
+
+namespace Nusantara.OpenGL;
+
+public class Buffer : GLObject
+{
+	internal Buffer(GL gl, uint handle) : base(gl, handle)
+	{
+		Debug.Assert(_gl.IsBuffer(_handle));
+	}
+
+	public Buffer(GL gl) : this(gl, Create(gl)) { }
+
+	private static uint Create(GL gl)
+	{
+		return gl.CreateBuffer();
+	}
+
+	internal override void Get(GLEnum pname, out int @params)
+	{
+		ThrowIfDisposed();
+
+		_gl.GetNamedBufferParameter(_handle, pname, out @params);
+	}
+	public void Get(BufferPNameARB pname, out int @params)
+	{
+		ThrowIfInvalidEnum(pname);
+
+		Get((GLEnum)pname, out @params);
+	}
+
+	protected override void Delete()
+	{
+		_gl.DeleteBuffer(_handle);
+	}
+}
---
+++ src/OpenGL/Nusantara.OpenGL/VertexArray.cs
@@ -0,0 +1,37 @@
+﻿using Silk.NET.OpenGL;
+using System.Diagnostics;
+
+namespace Nusantara.OpenGL;
+
+public class VertexArray : GLObject
+{
+	internal VertexArray(GL gl, uint handle) : base(gl, handle)
+	{
+		Debug.Assert(_gl.IsVertexArray(_handle));
+	}
+
+	public VertexArray(GL gl) : this(gl, Create(gl)) { }
+
+	private static uint Create(GL gl)
+	{
+		return gl.CreateVertexArray();
+	}
+
+	internal override void Get(GLEnum pname, out int param)
+	{
+		ThrowIfDisposed();
+
+		_gl.GetVertexArray(_handle, pname, out param);
+	}
+	public void Get(BufferPNameARB pname, out int param)
+	{
+		ThrowIfInvalidEnum(pname);
+
+		Get((GLEnum)pname, out param);
+	}
+
+	protected override void Delete()
+	{
+		_gl.DeleteVertexArray(_handle);
+	}
+}
