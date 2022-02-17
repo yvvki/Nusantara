38440c030efb1d0d3340e9572a4772cba3cd32c1
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-02-17 12:14:49 +0700

    Add Vertex struct.

---
+++ src/Core/Nusantara.Core/Vertex.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace Nusantara.Core;
+
+public record struct Vertex(Vector4 Position, Vector4 Normal, Vector2 UV)
+{
+	public Vertex(Vector3 Position, Vector3 Normal, Vector2 UV)
+		: this(
+			new Vector4(Position, 1),
+			new Vector4(Normal, 1),
+			UV)
+	{ }
+};
