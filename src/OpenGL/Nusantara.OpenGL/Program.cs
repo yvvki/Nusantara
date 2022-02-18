4f4c7cc4804cdd4710c4cdf745c51e0bb517e6c9
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-02-18 14:37:34 +0700

    Add Program and Shader class.

---
+++ src/OpenGL/Nusantara.OpenGL/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+using Silk.NET.OpenGL;
+
+namespace Nusantara.OpenGL;
+
+public class Program : GLObject
+{
+	internal Program(GL gl, uint handle) : base(gl, handle)
+	{
+		Debug.Assert(_gl.IsProgram(_handle));
+	}
+
+	public Program(GL gl) : this(gl, Create(gl)) { }
+
+	private static uint Create(GL gl)
+	{
+		return gl.CreateProgram();
+	}
+
+	internal override void Get(GLEnum pname, out int @params)
+	{
+		ThrowIfDisposed();
+
+		_gl.GetProgram(_handle, pname, out @params);
+	}
+	public void Get(BufferPNameARB pname, out int @params)
+	{
+		ThrowIfInvalidEnum(pname);
+
+		Get((GLEnum)pname, out @params);
+	}
+
+	protected override void Delete()
+	{
+		_gl.DeleteProgram(_handle);
+	}
+}
---
+++ src/OpenGL/Nusantara.OpenGL/Shader.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+using Silk.NET.OpenGL;
+
+namespace Nusantara.OpenGL;
+
+public class Shader: GLObject
+{
+	internal Shader(GL gl, uint handle) : base(gl, handle)
+	{
+		Debug.Assert(_gl.IsShader(_handle));
+	}
+
+	internal Shader(GL gl, GLEnum type) : this(gl, Create(gl, type)) { }
+
+	public Shader(GL gl, ShaderType type) : this(gl, Create(gl, type)) { }
+
+	private static uint Create(GL gl, GLEnum type)
+	{
+		return gl.CreateShader(type);
+	}
+
+	private static uint Create(GL gl, ShaderType type)
+	{
+		ThrowIfInvalidEnum(type);
+
+		return Create(gl, (GLEnum)type);
+	}
+
+	internal override void Get(GLEnum pname, out int param)
+	{
+		ThrowIfDisposed();
+
+		_gl.GetShader(_handle, pname, out param);
+	}
+	public void Get(BufferPNameARB pname, out int param)
+	{
+		ThrowIfInvalidEnum(pname);
+
+		Get((GLEnum)pname, out param);
+	}
+
+	protected override void Delete()
+	{
+		_gl.DeleteShader(_handle);
+	}
+}
