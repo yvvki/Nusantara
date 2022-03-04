f8f7e4b644e2e94daae5414ec75a9cef59dad646
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-04 10:13:42 +0700

    Expose Nusantara.OpenGL internals to Nusantara.Engine and add project references.

---
+++ src/API/Nusantara.OpenGL/Assembly.cs
@@ -0,0 +1,6 @@
+﻿// <https://github.com/YvvkiRika> wrote this file.
+// As long as you retain this notice, you can do whatever you want with this stuff.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Nusantara.Engine")]
