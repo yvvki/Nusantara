8e3e25ce8460a5b3c7ecc049ed2fb0a9784cfdc6
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-07 20:32:34 +0700

    Add Attrib and Mesh classes.

---
+++ src/Core/Nusantara.Engine/OpenGL/Attrib.cs
@@ -0,0 +1,42 @@
+﻿// <https://github.com/YvvkiRika> wrote this file.
+// As long as you retain this notice, you can do whatever you want with this stuff.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using Silk.NET.Maths;
+
+using VertexAttribType = Silk.NET.OpenGL.VertexAttribType;
+
+namespace Nusantara.Engine.OpenGL;
+
+public record Attrib(int Size, VertexAttribType Type)
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static VertexAttribType ParseType([NotNull] Type type)
+	{
+		return
+			  type == typeof(sbyte)
+			? VertexAttribType.Byte
+			: type == typeof(short)
+			? VertexAttribType.Short
+			: type == typeof(int) || type == typeof(long) || type == typeof(nint)
+			? VertexAttribType.Int
+			: type == typeof(byte)
+			? VertexAttribType.UnsignedByte
+			: type == typeof(ushort)
+			? VertexAttribType.UnsignedShort
+			: type == typeof(uint) || type == typeof(ulong) || type == typeof(nuint)
+			? VertexAttribType.UnsignedInt
+			: type == typeof(Half)
+			? VertexAttribType.HalfFloat
+			: type == typeof(float)
+			? VertexAttribType.Float
+			: type == typeof(double)
+			? VertexAttribType.Double
+			: throw new ArgumentException(
+				$"Unable to parse of type {type}.",
+				nameof(type));
+	}
+};
---
+++ src/Core/Nusantara.Engine/OpenGL/Mesh.cs
@@ -0,0 +1,66 @@
+﻿// <https://github.com/YvvkiRika> wrote this file.
+// As long as you retain this notice, you can do whatever you want with this stuff.
+
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+using Nusantara.OpenGL;
+
+using GL = Silk.NET.OpenGL.GL;
+using GLBuffer = Nusantara.OpenGL.Buffer;
+
+namespace Nusantara.Engine.OpenGL;
+
+public class Mesh
+{
+	public VertexArray VertexArray { get; }
+
+	public GLBuffer VertexBuffer { get; }
+	public GLBuffer ElementBuffer { get; }
+
+	public Texture[] Textures { get; init; }
+
+	public Mesh(GL gl, ReadOnlySpan<Vertex> vertices, ReadOnlySpan<uint> indices, Texture[] textures)
+	{
+		VertexBuffer = GLBuffer.FromData(gl, vertices);
+		ElementBuffer = GLBuffer.FromData(gl, indices);
+
+		Textures = textures;
+
+		// Binding buffers to VertexArray.
+		VertexArray = VertexArray.FromBuffers(
+			gl,
+			ElementBuffer,
+			0,
+			VertexBuffer,
+			0,
+			(uint)Marshal.SizeOf<Vertex>());
+
+		// Positions.
+		VertexArray.EnableAttribBindingFormat(
+			0,
+			0,
+			4,
+			Silk.NET.OpenGL.VertexAttribType.Float,
+			false,
+			(uint)Marshal.OffsetOf<Vertex>("Position"));
+
+		// Normals.
+		VertexArray.EnableAttribBindingFormat(
+			0,
+			0,
+			4,
+			Silk.NET.OpenGL.VertexAttribType.Float,
+			false,
+			(uint)Marshal.OffsetOf<Vertex>("Normal"));
+
+		// UVs.
+		VertexArray.EnableAttribBindingFormat(
+			0,
+			0,
+			2,
+			Silk.NET.OpenGL.VertexAttribType.Float,
+			false,
+			(uint)Marshal.OffsetOf<Vertex>("UV"));
+	}
+}
