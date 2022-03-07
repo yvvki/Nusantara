e6b9922af5140cef2b613643b3a6930c11ad003c
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-07 15:06:24 +0700

    Add VertexArrayExtensions.EnableAttribBindingFormat method.

---
+++ src/Core/Nusantara.Engine/OpenGL/VertexArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿// <https://github.com/YvvkiRika> wrote this file.
+// As long as you retain this notice, you can do whatever you want with this stuff.
+
+using Nusantara.OpenGL;
+
+using VertexAttribType = Silk.NET.OpenGL.VertexAttribType;
+
+namespace Nusantara.Engine.OpenGL;
+
+public static class VertexArrayExtensions
+{
+	public static void EnableAttribBindingFormat(
+		this VertexArray @this,
+		uint attribindex,
+		uint bindingindex,
+		int size,
+		VertexAttribType type,
+		bool normalized,
+		uint relativeoffset)
+	{
+		@this.AttribBinding(attribindex, bindingindex);
+		@this.AttribFormat(attribindex, size, type, normalized, relativeoffset);
+		@this.EnableAttrib(attribindex);
+	}
+}
