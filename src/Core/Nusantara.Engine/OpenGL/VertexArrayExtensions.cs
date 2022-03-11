8e668606d319820da2fb67d3e37bd82b81a3398a
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-11 08:21:55 +0700

    Add VertexArrayExtensions method that uses AttribHelper methods.

--- src/Core/Nusantara.Engine/OpenGL/VertexArrayExtensions.cs
+++ src/Core/Nusantara.Engine/OpenGL/VertexArrayExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// <https://github.com/YvvkiRika> wrote this file.
 // As long as you retain this notice, you can do whatever you want with this stuff.
 
+using System.Diagnostics.CodeAnalysis;
+
 using Nusantara.OpenGL;
 
 using VertexAttribType = Silk.NET.OpenGL.VertexAttribType;
@@ -9,14 +11,34 @@ namespace Nusantara.Engine.OpenGL;
 
 public static class VertexArrayExtensions
 {
+	public static void AttribFormat<T>(
+		this VertexArray @this,
+		uint attribindex,
+		bool normalized = false,
+		uint relativeoffset = 0)
+	{
+		@this.AttribFormat(attribindex, typeof(T), normalized, relativeoffset);
+	}
+
+	public static void AttribFormat(
+		this VertexArray @this,
+		uint attribindex,
+		[NotNull] Type type,
+		bool normalized = false,
+		uint relativeoffset = 0)
+	{
+		(int size, VertexAttribType @enum) = AttribHelper.GetSizeEnum(type);
+		@this.AttribFormat(attribindex, size, @enum, normalized, relativeoffset);
+	}
+
 	public static void EnableAttribBindingFormat(
 		this VertexArray @this,
 		uint attribindex,
 		uint bindingindex,
 		int size,
 		VertexAttribType type,
-		bool normalized,
-		uint relativeoffset)
+		bool normalized = false,
+		uint relativeoffset = 0)
 	{
 		@this.AttribBinding(attribindex, bindingindex);
 		@this.AttribFormat(attribindex, size, type, normalized, relativeoffset);
