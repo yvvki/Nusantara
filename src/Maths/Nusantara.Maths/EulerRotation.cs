9559bd9c9c9d3eeed93b33e72c0904ea2db065c4
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-11 10:34:45 +0700

    Fix sinp multiply with Y instead of X. Clean up.

--- src/Maths/Nusantara.Maths/EulerRotation.cs
+++ src/Maths/Nusantara.Maths/EulerRotation.cs
@@ -15,87 +15,8 @@ public record struct EulerRotation(float Yaw, float Pitch, float Roll)
 	public float Pitch = Pitch;
 	public float Roll = Roll;
 
-	// wikipedia
-	public static Quaternion ToQuaternionWikipedia(EulerRotation euler)
-	{
-		float sy, cy, sp, cp, sr, cr;
-
-		// Abbreviations for the various angular functions
-		float halfYaw = euler.Yaw * 0.5f;
-		cy = Cos(halfYaw);
-		sy = Sin(halfYaw);
-
-		float halfPitch = euler.Pitch * 0.5f;
-		cp = Cos(halfPitch);
-		sp = Sin(halfPitch);
-
-		float halfRoll = euler.Roll * 0.5f;
-		cr = Cos(halfRoll);
-		sr = Sin(halfRoll);
-
-		Quaternion q;
-
-		q.X = sr * cp * cy - cr * sp * sy;
-		q.Y = cr * sp * cy + sr * cp * sy;
-		q.Z = cr * cp * sy - sr * sp * cy;
-		q.W = cr * cp * cy + sr * sp * sy;
-
-		return q;
-	}
-	public static EulerRotation FromQuaternionWikipedia(Quaternion q)
-	{
-		EulerRotation euler;
-
-		// roll (x-axis rotation)
-		float sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
-		float cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-		euler.Roll = Atan2(sinr_cosp, cosr_cosp);
-
-		// pitch (y-axis rotation)
-		float sinp = 2 * (q.W * q.Y - q.Z * q.X);
-		if (Abs(sinp) >= 1)
-			euler.Pitch = CopySign(PI / 2, sinp); // use 90 degrees if out of range
-		else
-			euler.Pitch = Asin(sinp);
-
-		// yaw (z-axis rotation)
-		float siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
-		float cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-		euler.Yaw = Atan2(siny_cosp, cosy_cosp);
-
-		return euler;
-	}
-
-	// dotnet
-	public static Quaternion ToQuaternionDotnet(EulerRotation euler)
-	{
-		//  Roll first, about axis the object is facing, then
-		//  pitch upward, then yaw to face into the new heading
-		float sr, cr, sp, cp, sy, cy;
-
-		float halfRoll = euler.Roll * 0.5f;
-		sr = Sin(halfRoll);
-		cr = Cos(halfRoll);
-
-		float halfPitch = euler.Pitch * 0.5f;
-		sp = Sin(halfPitch);
-		cp = Cos(halfPitch);
-
-		float halfYaw = euler.Yaw * 0.5f;
-		sy = Sin(halfYaw);
-		cy = Cos(halfYaw);
-
-		Quaternion result;
-
-		result.Y = sy * cp * cr - cy * sp * sr;
-		result.X = cy * sp * cr + sy * cp * sr;
-		result.Z = cy * cp * sr - sy * sp * cr;
-		result.W = cy * cp * cr + sy * sp * sr;
-
-		return result;
-	}
 	// I am very smart yes.
-	public static EulerRotation FromQuaternionDotnet(Quaternion q)
+	public static EulerRotation CreateFromQuaternion(Quaternion q)
 	{
 		EulerRotation euler;
 
@@ -105,7 +26,7 @@ public record struct EulerRotation(float Yaw, float Pitch, float Roll)
 		euler.Yaw = Atan2(siny_cosp, cosy_cosp);
 
 		// Pitch (x-axis rotation)
-		float sinp = 2 * (q.W * q.X - q.Z * q.X);
+		float sinp = 2 * (q.W * q.X - q.Z * q.Y);
 		euler.Pitch = (Abs(sinp) >= 1) ? CopySign(PI / 2, sinp) : Asin(sinp);
 
 		// Roll (z-axis rotation)
@@ -116,8 +37,12 @@ public record struct EulerRotation(float Yaw, float Pitch, float Roll)
 		return euler;
 	}
 
-	public static implicit operator Quaternion(EulerRotation value)
+	public static explicit operator Quaternion(EulerRotation value)
 	{
 		return Quaternion.CreateFromYawPitchRoll(value.Yaw, value.Pitch, value.Roll);
+	}
+	public static explicit operator EulerRotation(Quaternion value)
+	{
+		return CreateFromQuaternion(value);
 	}
 };
--- src/Maths/Tests/Nusantara.Maths.Tests/Main.cs
+++ src/Maths/Tests/Nusantara.Maths.Tests/Main.cs
@@ -4,37 +4,14 @@ using Nusantara.Maths;
 
 Random r = new();
 
-TestWikipedia();
+EulerRotation e1 = new(r.NextSingle(), r.NextSingle(), r.NextSingle());
+Quaternion q1 = (Quaternion)(e1);
+
+EulerRotation e2 = (EulerRotation)(q1);
+Quaternion q2 = (Quaternion)(e2);
+
+Console.WriteLine(e1);
+Console.WriteLine(e2);
 Console.WriteLine();
-Console.WriteLine();
-TestDotnet();
-
-void TestWikipedia()
-{
-	EulerRotation e1 = new(r.NextSingle(), r.NextSingle(), r.NextSingle());
-	Quaternion q1 = EulerRotation.ToQuaternionWikipedia(e1);
-
-	EulerRotation e2 = EulerRotation.FromQuaternionWikipedia(q1);
-	Quaternion q2 = EulerRotation.ToQuaternionWikipedia(e2);
-
-	Console.WriteLine(e1);
-	Console.WriteLine(e2);
-	Console.WriteLine();
-	Console.WriteLine(q1);
-	Console.WriteLine(q2);
-}
-
-void TestDotnet()
-{
-	EulerRotation e1 = new(r.NextSingle(), r.NextSingle(), r.NextSingle());
-	Quaternion q1 = EulerRotation.ToQuaternionDotnet(e1);
-
-	EulerRotation e2 = EulerRotation.FromQuaternionDotnet(q1);
-	Quaternion q2 = EulerRotation.ToQuaternionDotnet(e2);
-
-	Console.WriteLine(e1);
-	Console.WriteLine(e2);
-	Console.WriteLine();
-	Console.WriteLine(q1);
-	Console.WriteLine(q2);
-}
+Console.WriteLine(q1);
+Console.WriteLine(q2);
