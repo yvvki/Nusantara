ed016562e9e53af8f3098b2f362368f877c93670
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-11 10:07:37 +0700

    Move tests to new Nusantara.Maths.Tests project.

--- src/Maths/Nusantara.Maths/EulerRotation.cs
+++ src/Maths/Nusantara.Maths/EulerRotation.cs
@@ -4,6 +4,8 @@
 using System.Diagnostics;
 using System.Numerics;
 
+using static System.MathF;
+
 namespace Nusantara.Maths;
 
 // Based on: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
@@ -14,120 +16,48 @@ public record struct EulerRotation(float Yaw, float Pitch, float Roll)
 	public float Roll = Roll;
 
 	// wikipedia
-	public static Quaternion ToQuaternionZYX(EulerRotation euler)
+	public static Quaternion ToQuaternionWikipedia(EulerRotation euler)
 	{
-		float sr, cr, sp, cp, sy, cy;
+		// Abbreviations for the various angular functions
+		float cy = Cos(euler.Yaw * 0.5f);
+		float sy = Sin(euler.Yaw * 0.5f);
+		float cp = Cos(euler.Pitch * 0.5f);
+		float sp = Sin(euler.Pitch * 0.5f);
+		float cr = Cos(euler.Roll * 0.5f);
+		float sr = Sin(euler.Roll * 0.5f);
 
-		float halfYaw = euler.Yaw * 0.5f;
-		sy = MathF.Sin(halfYaw);
-		cy = MathF.Cos(halfYaw);
+		Quaternion q;
+		q.W = cr * cp * cy + sr * sp * sy;
+		q.X = sr * cp * cy - cr * sp * sy;
+		q.Y = cr * sp * cy + sr * cp * sy;
+		q.Z = cr * cp * sy - sr * sp * cy;
 
-		float halfPitch = euler.Pitch * 0.5f;
-		sp = MathF.Sin(halfPitch);
-		cp = MathF.Cos(halfPitch);
-
-		float halfRoll = euler.Roll * 0.5f;
-		sr = MathF.Sin(halfRoll);
-		cr = MathF.Cos(halfRoll);
-
-		Quaternion result;
-
-		result.Z = sy * cp * cr - cy * sp * sr;
-		result.Y = cy * sp * cr + sy * cp * sr;
-		result.X = cy * cp * sr - sy * sp * cr;
-		result.W = cy * cp * cr + sy * sp * sr;
-
-		return result;
+		return q;
 	}
 
 	// wikipedia
-	public static EulerRotation CreateFromQuaternionZYX(Quaternion q)
+	public static EulerRotation FromQuaternionWikipedia(Quaternion q)
 	{
 		EulerRotation euler;
 
 		// roll (x-axis rotation)
 		float sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
 		float cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-		euler.Roll = MathF.Atan2(sinr_cosp, cosr_cosp);
+		euler.Roll = Atan2(sinr_cosp, cosr_cosp);
 
 		// pitch (y-axis rotation)
 		float sinp = 2 * (q.W * q.Y - q.Z * q.X);
-		if (MathF.Abs(sinp) >= 1)
-			euler.Pitch = MathF.CopySign(MathF.PI / 2, sinp); // use 90 degrees if out of range
+		if (Abs(sinp) >= 1)
+			euler.Pitch = CopySign(PI / 2, sinp); // use 90 degrees if out of range
 		else
-			euler.Pitch = MathF.Asin(sinp);
+			euler.Pitch = Asin(sinp);
 
 		// yaw (z-axis rotation)
 		float siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
 		float cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-		euler.Yaw = MathF.Atan2(siny_cosp, cosy_cosp);
+		euler.Yaw = Atan2(siny_cosp, cosy_cosp);
 
 		return euler;
-	}
-
-	// dotnet
-	public static Quaternion ToQuaternionYXZ(EulerRotation euler)
-	{
-		float sr, cr, sp, cp, sy, cy;
-
-		float halfYaw = euler.Yaw * 0.5f;
-		sy = MathF.Sin(halfYaw);
-		cy = MathF.Cos(halfYaw);
-
-		float halfPitch = euler.Pitch * 0.5f;
-		sp = MathF.Sin(halfPitch);
-		cp = MathF.Cos(halfPitch);
-
-		float halfRoll = euler.Roll * 0.5f;
-		sr = MathF.Sin(halfRoll);
-		cr = MathF.Cos(halfRoll);
-
-		Quaternion result;
-
-		result.Y = sy * cp * cr - cy * sp * sr;
-		result.X = cy * sp * cr + sy * cp * sr;
-		result.Z = cy * cp * sr - sy * sp * cr;
-		result.W = cy * cp * cr + sy * sp * sr;
-
-		return result;
-	}
-
-	// dotnet style
-	public static EulerRotation CreateFromQuaternionYXZ(Quaternion q)
-	{
-		EulerRotation euler;
-
-		// yaw (y-axis rotation)
-		float sinp = 2 * (q.W * q.Y - q.Z * q.X);
-		if (MathF.Abs(sinp) >= 1)
-			euler.Yaw = MathF.CopySign(MathF.PI / 2, sinp); // use 90 degrees if out of range
-		else
-			euler.Yaw = MathF.Asin(sinp);
-
-		// pitch (x-axis rotation)
-		float sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
-		float cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-		euler.Pitch = MathF.Atan2(sinr_cosp, cosr_cosp);
-
-		// roll (z-axis rotation)
-		float siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
-		float cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-		euler.Roll = MathF.Atan2(siny_cosp, cosy_cosp);
-
-		return euler;
-	}
-
-	public static void Test()
-	{
-		Random r = new();
-
-		Quaternion q1 = new(r.NextSingle(), r.NextSingle(), r.NextSingle(), r.NextSingle());
-
-		EulerRotation euler = CreateFromQuaternionYXZ(q1);
-
-		Quaternion q2 = ToQuaternionYXZ(euler);
-
-		Debug.Assert(q1.Equals(q2));
 	}
 
 	public static implicit operator Quaternion(EulerRotation value)
---
+++ src/Maths/Tests/Nusantara.Maths.Tests/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+using Nusantara.Maths;
+
+Random r = new();
+
+TestWikipedia();
+
+void TestWikipedia()
+{
+	EulerRotation e1 = new(r.NextSingle(), r.NextSingle(), r.NextSingle());
+	Quaternion q1 = EulerRotation.ToQuaternionWikipedia(e1);
+
+	EulerRotation e2 = EulerRotation.FromQuaternionWikipedia(q1);
+	Quaternion q2 = EulerRotation.ToQuaternionWikipedia(e2);
+
+	Console.WriteLine(e1);
+	Console.WriteLine(e2);
+	Console.WriteLine();
+	Console.WriteLine(q1);
+	Console.WriteLine(q2);
+}
+
+void TestDotNet()
+{
+
+}
