59d52807630efaf0b2eeb662654766ef556266a5
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-14 11:13:11 +0700

    Remove ToEulerAngles method.

--- src/Maths/Nusantara.Maths/MathHelper.cs
+++ src/Maths/Nusantara.Maths/MathHelper.cs
@@ -99,35 +99,6 @@ public static partial class MathHelper
 
 	#endregion
 
-	#region To Euler Angles
-
-	// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Source_code_2
-	public static (float yaw, float pitch, float roll) ToEulerAngles(Quaternion q)
-	{
-		float yaw, pitch, roll;
-
-		// roll (x-axis rotation)
-		float sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
-		float cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-		roll = MathF.Atan2(sinr_cosp, cosr_cosp);
-
-		// pitch (y-axis rotation)
-		float sinp = 2 * (q.W * q.Y - q.Z * q.X);
-		if (MathF.Abs(sinp) >= 1)
-			pitch = MathF.CopySign(MathF.PI / 2, sinp); // use 90 degrees if out of range
-		else
-			pitch = MathF.Asin(sinp);
-
-		// yaw (z-axis rotation)
-		float siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
-		float cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-		yaw = MathF.Atan2(siny_cosp, cosy_cosp);
-
-		return (yaw, pitch, roll);
-	}
-
-	#endregion
-
 	#region Create Matrix
 
 	/// <inheritdoc cref="Matrix4x4.CreateScale(Vector3)"/>
