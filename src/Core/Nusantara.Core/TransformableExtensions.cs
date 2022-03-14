d6df1550921d802f4a023e96a931a1184f9aa2b3
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-14 11:03:39 +0700

    Rename Transformable to TransformableExtensions and remove redundant methods.

---
+++ src/Core/Nusantara.Core/TransformableExtensions.cs
@@ -0,0 +1,39 @@
+﻿// <https://github.com/YvvkiRika> wrote this file.
+// As long as you retain this notice, you can do whatever you want with this stuff.
+
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+using Nusantara.Maths;
+
+namespace Nusantara;
+
+public static class TransformableExtensions
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void Negate<T>(this ref T transformable)
+		where T : struct, ITransformable
+	{
+		transformable.Translation = Vector3.Negate(transformable.Translation);
+		transformable.Rotation = Quaternion.Inverse(transformable.Rotation);
+		transformable.Scale = Vector3.Divide(Vector3.One, transformable.Scale);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void Concatenate<T>(this ref T @this, ITransformable other)
+		where T : struct, ITransformable
+	{
+		@this.Translation = Vector3.Add(@this.Translation, other.Translation);
+		@this.Rotation = Quaternion.Concatenate(@this.Rotation, other.Rotation);
+		@this.Scale = Vector3.Multiply(@this.Scale, other.Scale);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static Matrix4x4 GetMatrix(this ITransformable transformable)
+	{
+		return MathHelper.CreateTransform(
+			transformable.Translation,
+			transformable.Rotation,
+			transformable.Scale);
+	}
+}
