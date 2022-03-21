337b0b3d20e6c12741d3204223c49c626efcd968
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-21 12:28:21 +0700

    Add GLTF class.

---
+++ src/Core/Nusantara.Engine/GLTF/GLTF.cs
@@ -0,0 +1,9 @@
+﻿// <https://github.com/YvvkiRika> wrote this file.
+// As long as you retain this notice, you can do whatever you want with this stuff.
+
+namespace Nusantara.Engine.GLTF;
+
+public static class GLTF
+{
+
+}
