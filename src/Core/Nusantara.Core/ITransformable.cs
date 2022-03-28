7e4b41d1294fb5698e4ec291c579d5ab772e613e
Yuuki Rika <69618058+YvvkiRika@users.noreply.github.com>
2022-03-28 18:31:02 +0700

    Add directions in ITransformable, that is transformed with rotation.

--- src/Core/Nusantara.Core/ITransformable.cs
+++ src/Core/Nusantara.Core/ITransformable.cs
@@ -10,4 +10,11 @@ public interface ITransformable
 	Vector3 Translation { get; set; }
 	Quaternion Rotation { get; set; }
 	Vector3 Scale { get; set; }
+
+	Vector3 Right
+		=> Vector3.Transform(Directions.Right, Rotation);
+	Vector3 Forward
+		=> Vector3.Transform(Directions.Forward, Rotation);
+	Vector3 Up
+		=> Vector3.Transform(Directions.Up, Rotation);
 }
