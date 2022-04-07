84d5e73083018017ca041a945e84bc9eb87fbe8c
Yuuki Rika <yuuki.rikachan@gmail.com>
2022-04-07 15:06:44 +0700

    Add experimental methods.

--- src/Core/Nusantara.Engine/OpenGL/VertexArrayExtensions.cs
+++ src/Core/Nusantara.Engine/OpenGL/VertexArrayExtensions.cs
@@ -2,6 +2,8 @@
 // As long as you retain this notice, you can do whatever you want with this stuff.
 
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 using Silk.NET.OpenGL;
 
@@ -66,5 +68,60 @@ public static class VertexArrayExtensions
 		@this.AttribBinding(attribindex, bindingindex);
 		@this.AttribFormat(attribindex, size, type, normalized, relativeoffset);
 		@this.EnableAttrib(attribindex);
+	}
+
+	// Experimental methods
+	// Handle attrib from user struct types.
+	public static void AttribFormatStruct<T>(
+		this VertexArray @this,
+		uint attribindexoffset,
+		bool normalized = false,
+		uint relativeoffset = 0)
+		where T : struct
+	{
+		@this.AttribFormatStruct(attribindexoffset, typeof(T), normalized, relativeoffset);
+	}
+
+	public static void AttribFormatStruct(
+		this VertexArray @this,
+		uint attribindexoffset,
+		[NotNull] Type type,
+		bool normalized = false,
+		uint relativeoffset = 0)
+	{
+		FieldInfo[] typeFields = type.GetFields();
+
+		for (uint i = 0; i < typeFields.Length; i++)
+		{
+			@this.AttribFormat(
+				attribindexoffset + i,
+				typeFields[i].FieldType,
+				false,
+				(uint)Marshal.OffsetOf(type, typeFields[i].Name));
+		}
+	}
+
+	public static void EnableAttribBindingFormatStruct<T>(
+		this VertexArray @this,
+		uint attribindexoffset,
+		uint bindingindex,
+		bool normalized = false,
+		uint relativeoffset = 0)
+		where T : struct
+	{
+		@this.EnableAttribBindingFormatStruct(attribindexoffset, bindingindex, typeof(T), normalized, relativeoffset);
+	}
+
+	public static void EnableAttribBindingFormatStruct(
+		this VertexArray @this,
+		uint attribindexoffset,
+		uint bindingindex,
+		[NotNull] Type type,
+		bool normalized = false,
+		uint relativeoffset = 0)
+	{
+		@this.AttribBinding(attribindexoffset, bindingindex);
+		@this.AttribFormatStruct(attribindexoffset, type, normalized, relativeoffset);
+		@this.EnableAttrib(attribindexoffset);
 	}
 }
